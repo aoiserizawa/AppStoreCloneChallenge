ab1c9bd220d4610db6bbd1c59c55ab8b0d033f82
alvin joseph valdez <alvinjosephvaldez@Macintoshs-MacBook-Pro-3.local>
2018-11-02 11:16:07 +0800

    Initial Commit

---
+++ Appstore Clone Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Appstore Clone Challenge
+//
+//  Created by alvin joseph valdez on 02/11/2018.
+//  Copyright © 2018 alvin joseph valdez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Appstore Clone ChallengeTests/Appstore_Clone_ChallengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Appstore_Clone_ChallengeTests.swift
+//  Appstore Clone ChallengeTests
+//
+//  Created by alvin joseph valdez on 02/11/2018.
+//  Copyright © 2018 alvin joseph valdez. All rights reserved.
+//
+
+import XCTest
+@testable import Appstore_Clone_Challenge
+
+class Appstore_Clone_ChallengeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
